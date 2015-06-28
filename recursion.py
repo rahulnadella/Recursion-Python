2a787bbb9b1e76a57d0efc08c84b4bf25634b57d
Rahul Nadella <rahulnadella@yahoo.com>
2015-06-28 12:37:46 -0400

    Updated comments to follow Python standard

--- recursion.py
+++ recursion.py
@@ -26,8 +26,6 @@ In mathematics, the factorial of a non-negative integer n, denoted by n!,
 is the product of all positive integers less than or equal to n. For
 example, The value of 0! is 1, according to the convention for an empty
 product.
-
-@return The Factorial value of integer (4! = 24)
 """
 
 
